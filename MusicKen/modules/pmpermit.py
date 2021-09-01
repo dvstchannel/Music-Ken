57948bd44659e5db0b76bdfc911026effc372e1e
dvstchannel <83810390+dvstchannel@users.noreply.github.com>
2021-09-01 19:20:13 +0700

    Update pmpermit.py

--- MusicKen/modules/pmpermit.py
+++ MusicKen/modules/pmpermit.py
@@ -24,7 +24,7 @@ async def pmPermit(client: USER, message: Message):
             return
         await USER.send_message(
             message.chat.id,
-            f"""**Hay saya adalah Layanan**\n╭━━━━━━━━━━━━━━━━━╮\n┣**Assistant [{PROJECT_NAME}](https://t.me/{BOT_USERNAME})**\n╰━━━━━━━━━━━━━━━━━╯\n**❗️ Rules:**\n- Jangan Spam Pesan disini\n- Jangan Spam Lagu Biar Ga Error\n- Tutorial Cara Menggunakan bot Lihat di @{UPDATES_CHANNEL}\n\n━━━━━━━━━━━━━━━━━━━━━━\n**👉 KIRIM LINK INVITE ATAU USERNAME GRUP, JIKA ASSISTANT TIDAK DAPAT BERGABUNG DENGAN GRUP ANDA.**\n▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n**💬 GROUP  :** @{SUPPORT_GROUP}\n**👮 OWNER :** @{OWNER}\n▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰""",
+            f"""**Xin chào tôi là dịch vụ**\n╭━━━━━━━━━━━━━━━━━╮\n┣**Phụ tá [{PROJECT_NAME}](https://t.me/{BOT_USERNAME})**\n╰━━━━━━━━━━━━━━━━━╯\n**❗️ Quy tắc:**\n- Đừng Spam Đặt hàng tại đây\n- Không spam bài hát nên không có lỗi\n- Hướng dẫn Cách sử dụng bot Nhìn vào @{UPDATES_CHANNEL}\n\n━━━━━━━━━━━━━━━━━━━━━━\n**👉 GỬI LIÊN KẾT MỜI HOẶC TÊN NGƯỜI DÙNG NHÓM, NẾU NGƯỜI HỖ TRỢ KHÔNG THỂ THAM GIA NHÓM CỦA BẠN.**\n▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n**💬 GROUP  :** @{SUPPORT_GROUP}\n**👮 OWNER :** @{OWNER}\n▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰""",
             disable_web_page_preview=True,
         )
         return
@@ -38,11 +38,11 @@ async def bye(client: Client, message: Message):
         queryy = text[1]
         if queryy == "on":
             PMSET = True
-            await message.reply_text("**Pmpermit dinyalakan**")
+            await message.reply_text("**ĐÃ CHO PHÉP NHẮN TIN**")
             return
         if queryy == "off":
             PMSET = None
-            await message.reply_text("**Pmpermit dimatikan**")
+            await message.reply_text("**KHÔNG CHO PHÉP NHẮN TIN**")
             return
 
 
@@ -51,7 +51,7 @@ async def autopmPermiat(client: USER, message: Message):
     chat_id = message.chat.id
     if not chat_id in pchats:
         pchats.append(chat_id)
-        await message.reply_text("Disetujui untuk Private Message")
+        await message.reply_text("Được chấp thuận cho tin nhắn riêng tư")
         return
     message.continue_propagation()
 
@@ -61,7 +61,7 @@ async def pmPermiat(client: USER, message: Message):
     chat_id = message.chat.id
     if not chat_id in pchats:
         pchats.append(chat_id)
-        await message.reply_text("Disetujui untuk Private Message")
+        await message.reply_text("Được chấp thuận cho tin nhắn riêng tư")
         return
     message.continue_propagation()
 
@@ -71,6 +71,6 @@ async def rmpmPermiat(client: USER, message: Message):
     chat_id = message.chat.id
     if chat_id in pchats:
         pchats.remove(chat_id)
-        await message.reply_text("Maaf anda Ditolak untuk Private Message")
+        await message.reply_text("Tin nhắn riêng")
         return
     message.continue_propagation()
